a99d0c85215ee426da14cc33ba478de88b4bfc83
MasterFlo-droid <77565066+MasterFlo-droid@users.noreply.github.com>
2021-03-15 09:38:24 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
+    if(array !== undefined && array.length !== 0){
+  return Math.min.apply(null, array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+    if(array !== undefined && array.length !== 0){
+     return Math.max.apply(null, array);  
+}
   return 0;
 }
 
 exports.avg = function avg (array) {
+    if(array !== undefined && array.length !== 0){
+    let sum = 0;
+    for( let i of array){
+      sum += i;
+    };
+    return sum / array.length;
+  }
   return 0;
 }
+
